e16bb2f63b36c77a036ae74af9f7544c1bf6a88f
Tanisha Bisht <agamyatani@gmail.com>
2025-02-07 18:01:29 -0500

    add ClangFormat, ClangTidy, CMake, GitIgnore

---
+++ cpp-template-repo/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  if (x > 0) {
+    cout << "Hello";
+  }
+  for (int i = 0; i < 5; i++) {
+    cout << "Test";
+  }
+  return 0;
+}
